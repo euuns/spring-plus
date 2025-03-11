ebdfdc5fe8756377a494dab6a172cb72fb5b66d8
euuns <dmsu2@naver.com>
2025-03-11 17:06:55 +0900

    Lv1(3): 할 일 검색 시 weather조건, 기간 조건 추가
    1. 검색 기간x weather 조건x: ORDER BY modifiedAt DESC
    2. 검색 기간x weather 조건o: WHERE weather = ? ORDER BY modifiedAt DESC
    3. 검색 기간o weather 조건x: WHERE modifiedAt BETWEEN ? AND ? ORDER BY modifiedAt DESC
    4. 검색 기간o weather 조건o: WHERE weather = ? AND modifiedAt BETWEEN ? AND ? ORDER BY modifiedAt DESC
    
    5. 종료일 null: endedAt = LocalDate.now()
    6. 시작일 null: startedAt = LocalDate.MIN

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u WHERE t.weather = :weather ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByWeatherOrderByModifiedAt(String weather, Pageable pageable);
+
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE t.modifiedAt BETWEEN :startedAt AND :endedAt ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByDateRange(LocalDate startedAt, LocalDate endedAt, Pageable pageable);
+
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE t.weather = :weather " +
+            "AND t.modifiedAt BETWEEN :startedAt AND :endedAt " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findTodosByWeatherAndDateRange(String weather, LocalDate startedAt, LocalDate endedAt, Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
