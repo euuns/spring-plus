f3fccd880c2425dd4cc93b8189bca2779aff6f49
euuns <dmsu2@naver.com>
2025-03-11 20:23:26 +0900

    Lv2(8): Query DSL 사용

---
+++ src/test/java/org/example/expert/domain/todo/repository/TodoDslRepositoryImplTest.java
@@ -0,0 +1,48 @@
+package org.example.expert.domain.todo.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TodoDslRepositoryImplTest {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private Todo todo;
+    private User user;
+
+    @BeforeEach
+    void setup() {
+        user = new User("email@email.com", "pass1234!", UserRole.USER, "nickname");
+        User saveUser = userRepository.save(user);
+        todo = new Todo("title", "contents", "weather", saveUser);
+        todoRepository.save(todo);
+    }
+
+    @Test
+    public void findByIdWithUserTest() {
+        Optional<Todo> getTodo = todoRepository.findByIdWithUser(1L);
+        assertEquals(getTodo.get().getUser().getEmail(), user.getEmail());
+    }
+}
