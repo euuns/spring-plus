4dda77f40e3e53b340f552465f47aeacd26b6b9b
euuns <dmsu2@naver.com>
2025-03-18 19:51:05 +0900

    Fix: test용 h2 설정 지정

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -2,10 +2,12 @@ package org.example.expert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = ExpertApplication.class)
 class ExpertApplicationTests {
 
